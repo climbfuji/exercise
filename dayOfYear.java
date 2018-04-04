df9ef5ddb0ff22c18e348083a420d792a575dab9
Dom Heinzeller <dom.heinzeller@noaa.gov>
2018-04-04 10:55:00 -0600

    Improved version of Julian day (of year)

--- dayOfYear.java
+++ dayOfYear.java
@@ -1,26 +1,9 @@
 public static int dayOfYear(int month, int dayOfMonth, int year) {
-    if (month == 2) {
-        dayOfMonth += 31;
-    } else if (month == 3) {
-        dayOfMonth += 59;
-    } else if (month == 4) {
-        dayOfMonth += 90;
-    } else if (month == 5) {
-        dayOfMonth += 31 + 28 + 31 + 30;
-    } else if (month == 6) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31;
-    } else if (month == 7) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30;
-    } else if (month == 8) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31;
-    } else if (month == 9) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-    } else if (month == 10) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-    } else if (month == 11) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-    } else if (month == 12) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31;
-    }
+    import java.util.GregorianCalendar;
+    GregorianCalendar gc = new GregorianCalendar();
+    gc.set(GregorianCalendar.DAY_OF_MONTH, 8);
+    gc.set(GregorianCalendar.MONTH, GregorianCalendar.JUNE);
+    gc.set(GregorianCalendar.YEAR, 2010);
+    dayOfMonth = gc.get(GregorianCalendar.DAY_OF_YEAR);
     return dayOfMonth;
 }
